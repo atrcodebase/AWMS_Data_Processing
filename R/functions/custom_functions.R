94c960c01bfff02fb9acadbb311841e41711d5f2
Shahim Nadery <Shahim_Nadery@yahoo.com>
2022-07-16 22:22:49 +0430

    Fixed bugs in relevancy script!

--- R/functions/custom_functions.R
+++ R/functions/custom_functions.R
@@ -31,3 +31,110 @@ compare_dt <- function (df1, df2, unique_id_df1, unique_id_df2, compare_all = TR
     return(diff)
   }
 }
+
+# Relevancy Function -------------------------------------------------------------------------------
+relevancy_check <- function(data, tool_relevancy, sheet){
+  # start.time <- Sys.time()
+  tool_sub <- tool_relevancy %>% 
+    filter(name %in% names(data)) 
+  
+  relevancy_log <- data.frame(KEY=NA,question=NA,q_val=NA,relevancy=NA,relevant_q=NA,
+                              relev_val=NA)
+  #loop through rows and tool
+  for(i in 1:nrow(data)){
+    
+    prev_log_res <- NA
+    final_match <- NA
+    for(j in 1:nrow(tool_sub)){
+      var_name <- tool_sub$name[j]
+      
+      data_q_val <- data[[var_name]][i]
+      
+      if(!is.na(data_q_val)){
+        
+        split_opr <- str_split(tool_sub$operator[j], " - ", simplify = T)
+        # log_opr <- tool_sub$logical_opr[j]
+        split_log_opr <- str_split(tool_sub$logical_opr[j], " - ", simplify = T)
+        opr_order <- tool_sub$operator_order[j]
+        relev_var <- tool_sub$q[j]
+        relev_var_val <- tool_sub$val[j]
+        rep <- tool_sub$rep[j]
+        last_rep <- tool_sub$last_rep[j]
+        #Data relevant question value
+        dt_relev_q_val <- data[[relev_var]][i]
+        if(is.na(dt_relev_q_val)){
+          dt_relev_q_val <- ""
+        }
+        
+        #Using grepl for "Selected()" relevancies 
+        if(split_opr[1] == "selected"){
+          
+          curr_log_res <- grepl(relev_var_val, dt_relev_q_val)
+          
+        } else if(grepl(" - ", relev_var_val)){
+          
+          curr_log_res <- multi_val(dt_relev_q_val, relev_var_val, split_opr, relev_var, split_val, split_log_opr)
+          
+        } else if(grepl("\\$\\{", relev_var_val)){
+          
+          relev_var_val <- str_extract(relev_var_val, "(?<=\\$\\{)(.*?)(?=\\})")
+          curr_log_res <- match.fun(split_opr[1])(dt_relev_q_val, data[[relev_var_val]][i])
+          
+        } else {
+          #in case there are 3 values
+          if(length(split_opr) == 2 & rep > 1){
+            curr_log_res <- match.fun(split_opr[2])(dt_relev_q_val, relev_var_val)
+          } else {
+            curr_log_res <- match.fun(split_opr[1])(dt_relev_q_val, relev_var_val)
+          }
+        }
+        #For questions that has more than one relevancy
+        if(rep > 1){
+          final_match <- match.fun(split_log_opr[1])(prev_log_res, curr_log_res)
+        } else {
+          final_match <- curr_log_res
+        }
+        
+        
+        if(last_rep & !final_match){
+          log <- c(data$KEY[i], var_name, data_q_val, tool_sub$relevance[j], tool_sub$q[j], dt_relev_q_val)
+          relevancy_log <- rbind(relevancy_log, log)
+        }
+        #storing Current Logical Result for next check
+        prev_log_res <- curr_log_res
+      }
+    }
+  }
+  relevancy_log <- relevancy_log[-1,] %>% 
+    mutate(sheet_name = sheet)
+  
+  if(nrow(relevancy_log) == 0){
+    print(paste0("No relevancy related issues found in: ", sheet))
+  } else {
+    print(paste0("Relevancy issues found in: ", sheet))
+  }
+  # end.time <- Sys.time()
+  # time.taken <- end.time - start.time
+  # print(time.taken)
+  return(relevancy_log)
+}
+multi_val <- function(data, relev_var_val, split_opr, relev_var, split_val, split_log_opr){
+  split_val <- str_split(relev_var_val, " - ", simplify = T)
+  
+  check1 <- match.fun(split_opr[1])(data, split_val[1])
+  check2 <- match.fun(split_opr[2])(data, split_val[2])
+  
+  #in case there are 3 values
+  if(length(split_val) == 3){
+    curr_log_res <- match.fun(split_log_opr[1])(check1, check2)
+    #re-calculting current logical result with the third value
+    check3 <- match.fun(split_opr[3])(data, split_val[3])
+    curr_log_res <- match.fun(split_log_opr[2])(curr_log_res, check3)
+    
+  } else {
+    curr_log_res <- match.fun(split_log_opr[1])(check1, check2)
+  }
+  return(curr_log_res)
+}
+
+
--- R/relevancy_check.R
+++ R/relevancy_check.R
@@ -1,106 +1,16 @@
-# Function -------------------------------------------------------------------------------
-relevancy_check <- function(data, tool_relevancy, sheet){
-  # start.time <- Sys.time()
-  tool_sub <- tool_relevancy %>% 
-    filter(name %in% names(data)) 
-  
-  relevancy_log <- data.frame(KEY=NA,question=NA,q_val=NA,relevancy=NA,relevant_q=NA,
-                              relev_val=NA)
-  #loop through rows and tool
-  for(i in 1:nrow(data)){
-    
-    prev_log_res <- NA
-    final_match <- NA
-    for(j in 1:nrow(tool_sub)){
-      var_name <- tool_sub$name[j]
-      
-      data_q_val <- data[[var_name]][i]
-      
-      if(!is.na(data_q_val)){
-        
-        split_opr <- str_split(tool_sub$operator[j], " - ", simplify = T)
-        # log_opr <- tool_sub$logical_opr[j]
-        split_log_opr <- str_split(tool_sub$logical_opr[j], " - ", simplify = T)
-        opr_order <- tool_sub$operator_order[j]
-        relev_var <- tool_sub$q[j]
-        relev_var_val <- tool_sub$val[j]
-        rep <- tool_sub$rep[j]
-        last_rep <- tool_sub$last_rep[j]
-        #Data relevant question value
-        dt_relev_q_val <- data[[relev_var]][i]
-        
-        #Using grepl for "Selected()" relevancies 
-        if(split_opr[1] == "selected"){
-          
-          curr_log_res <- grepl(relev_var_val, dt_relev_q_val)
-          
-        } else if(grepl(" - ", relev_var_val)){
-          
-          curr_log_res <- multi_val(dt_relev_q_val, relev_var_val, split_opr, relev_var, split_val, split_log_opr)
-          
-        } else if(grepl("\\$\\{", relev_var_val)){
-          
-          relev_var_val <- str_extract(relev_var_val, "(?<=\\$\\{)(.*?)(?=\\})")
-          curr_log_res <- match.fun(split_opr[1])(dt_relev_q_val, data[[relev_var_val]][i])
-          
-        } else {
-          #in case there are 3 values
-          if(length(split_opr) == 2 & rep > 1){
-            curr_log_res <- match.fun(split_opr[2])(dt_relev_q_val, relev_var_val)
-          } else {
-            curr_log_res <- match.fun(split_opr[1])(dt_relev_q_val, relev_var_val)
-          }
-        }
-        #For questions that has more than one relevancy
-        if(rep > 1){
-          final_match <- match.fun(split_log_opr[1])(prev_log_res, curr_log_res)
-        } else {
-          final_match <- curr_log_res
-        }
-        
-        
-        if(last_rep & !final_match){
-          log <- c(data$KEY[i], var_name, data_q_val, tool_sub$relevance[j], tool_sub$q[j], dt_relev_q_val)
-          relevancy_log <- rbind(relevancy_log, log)
-        }
-        #storing Current Logical Result for next check
-        prev_log_res <- curr_log_res
-      }
-    }
-  }
-  relevancy_log <- relevancy_log[-1,] %>% 
-    mutate(sheet_name = sheet)
-  
-  if(nrow(relevancy_log) == 0){
-    print(paste0("No relevancy related issues found in: ", sheet))
-  } else {
-    print(paste0("Relevancy issues found in: ", sheet))
-  }
-  # end.time <- Sys.time()
-  # time.taken <- end.time - start.time
-  # print(time.taken)
-  return(relevancy_log)
-}
-multi_val <- function(data, relev_var_val, split_opr, relev_var, split_val, split_log_opr){
-  split_val <- str_split(relev_var_val, " - ", simplify = T)
-  
-  check1 <- match.fun(split_opr[1])(data, split_val[1])
-  check2 <- match.fun(split_opr[2])(data, split_val[2])
-  
-  #in case there are 3 values
-  if(length(split_val) == 3){
-    curr_log_res <- match.fun(split_log_opr[1])(check1, check2)
-    #re-calculting current logical result with the third value
-    check3 <- match.fun(split_opr[3])(data, split_val[3])
-    curr_log_res <- match.fun(split_log_opr[2])(curr_log_res, check3)
-    
-  } else {
-    curr_log_res <- match.fun(split_log_opr[1])(check1, check2)
-  }
-  return(curr_log_res)
-}
+# Install/load required packages ---------------------------------------------------------
+if(!require(stringr)) install.packages("stringr")
+if(!require(readxl)) install.packages("readxl")
+if(!require(dplyr)) install.packages("dplyr")
+if(!require(glue)) install.packages("glue")
+if(!require(writexl)) install.packages("writexl")
+if(!require(tidyr)) install.packages("tidyr")
+source("R/functions/custom_functions.R")
 
-# Read Tool ------------------------------------------------------------------------------
+# Read data ------------------------------------------------------------------------------
+data_path <- "output/cleaned_approved_data/" # data path
+convert_to_na <- c("NA", "N/A", "-", " ") # values to convert to NA
+source("R/read_data.R") # read data
 tool_relevancy <- read_excel("input/tools/tool_relevancies.xlsx")
 
 #apply function --------------------------------------------------------------------------
@@ -131,6 +41,9 @@ relevancy_issues <- rbind(
   Market_relevancy,
   Closing_Group_relevancy
 )
+#temporary
+relevancy_issues <- relevancy_issues %>% 
+  filter(!(relevant_q %in% "HH_Mem_Age" &  as.numeric(relev_val) >= 18))
 
 #Export ----------------------------------------------------------------------------------
 writexl::write_xlsx(relevancy_issues, "output/Tool_relevancy_issues.xlsx")
